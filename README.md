3f73cd570a58ba7e5fe28136cf15f909ac473aab
Jason Ho <jasoryeh@gmail.com>
2018-08-17 22:16:36 -0700

    FIRST

---
+++ README.md
@@ -0,0 +1,7 @@
+# brolwser
+
+I thought about the Steam in-game browser, and just wanted to make a somewhat better, simple browser for in-game usage. (Yes I know there are people with two monitors)
+
+### License
+
+Haven't decided to license yet since it is not done yet.
---
+++ src/main.js
@@ -0,0 +1,121 @@
+const {app, BrowserWindow, globalShortcut, ipcMain} = require('electron')
+const nativeImage = require('electron').nativeImage
+const path = require('path')
+const url = require('url')
+
+var child = require('child_process').execFile;
+
+let win
+
+function createWindow() {
+    
+    console.log("Creating window")
+    
+    win = new BrowserWindow({
+        title: "brolwser",
+        //icon: path.join(__dirname, 'assets/icon.png'),
+        frame: false
+        //transparent: true,
+        //alwaysOnTop: true
+    })
+    
+    console.log("Maximizing, and hiding unwanted UI")
+    
+    win.maximize()
+    win.setMenuBarVisibility(false)
+    
+    console.log("Loading web pages")
+    
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+    
+    // win.setIgnoreMouseEvents(true);
+    
+    /* load other external urls.
+    win.loadURL(url.format({
+        pathname: "jasoryeh.tk",
+        protocol: 'https:',
+        slashes: true
+    }))*/
+    
+    //open dev-tools win.webContents.openDevTools()
+    
+    console.log("Registering close events")
+    
+    win.on('closed', () => {
+        
+        win = null
+    
+    })
+    
+    // add window mods here.
+    
+    console.log("Reloading window properties")
+    win.reload()
+    
+    console.log("Registering escape shortcut")
+    globalShortcut.register('Escape', function(){
+        win = null
+        app.quit()
+    })
+    globalShortcut.register('1', function(){
+        win.loadURL(url.format({
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        }))
+    })
+    globalShortcut.register('2', function(){
+        win.loadURL(url.format({
+            pathname: path.join(__dirname, 'overlay.html'),
+            protocol: 'file:',
+            slashes: true
+        }))
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if(win === null) {
+        createWindow()
+    }
+})
+
+var clickThrough = false;
+
+ipcMain.on('toggle-clickthrough', (event, arg) => {
+    //console.log(arg);
+    if(arg === "get") {
+        //console.log("returning");
+        event.returnValue = clickThrough.toString;
+        return;
+    }
+    if(arg === "true") {
+        clickThrough = true;
+    } else if (arg === "false") {
+        clickThrough = false;
+    } else {
+        clickThrough = !clickThrough;
+    }
+    //console.log(clickThrough);
+    //console.log("Recieved wnbu");
+    if(win === null || win === undefined) return;
+    win.setIgnoreMouseEvents(clickThrough, {forward: true});
+})
+
+ipcMain.on('run-program', (event, arg) => {
+    child(arg, "", (err, data) => {
+        console.log(err);
+        console.log(data);
+    });
+});
